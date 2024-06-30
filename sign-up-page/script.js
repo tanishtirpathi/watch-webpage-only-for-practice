c1bc995cc105c2f3df2540dce1794a977793e3d3
Tanish Tirpathi's JARVIs <tanishtirpathi0@gmail.com>
2024-06-30 05:47:19 +0530

    update the form and add a image

--- sign-up-page/script.js
+++ sign-up-page/script.js
@@ -1,15 +1,18 @@
-const form = document.getElementById("signup-form");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  // Simulate form submission (replace with your actual logic)
-  alert("Form submitted successfully!");
-  window.location.href = "/index.html";
-});
-
 const contain = document.getElementById("hde");
-
+const sign = document.getElementById("signup-form");
 document.addEventListener("DOMContentLoaded", (event) => {
   contain.classList.add("container");
+});
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzIyBk-3vh1qI7mTnV4eNN2XSET6J3LIcBNll7CuSrejIts1YBxFHP4NZoEGz1jW1Hz-w/exec";
+const form = document.forms["sign-up form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => alert("Form submitted successfully!"))
+
+    .catch((error) => console.error("Error!", error.message));
+  form.reset();
 });
